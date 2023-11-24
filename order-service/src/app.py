8235be4625b7c6c6857d6ccd27e400b9ebc0fd37
kloe-b <burazerikloena@gmail.com>
2023-11-24 09:34:03 +0700

    resructure code

--- order-service/src/app.py
+++ order-service/src/app.py
@@ -1,32 +1,81 @@
 from flask import Flask, jsonify, request
 import os
+import json
 from flask_cors import CORS
-from order.order_service import order_service
-from database import db, bcrypt
+from database import db,Order, bcrypt
+from redis import Redis
+import subprocess
+import logging
+import time
+import threading
+from threading import Thread
 
-def create_app() -> Flask:
-    """Create flask app."""
-    app = Flask(__name__)
-    
-    CORS(app)
-    app.secret_key = os.environ.get("SECRET_KEY", 'your_secret_key')
-    app.register_blueprint(order_service)
+app = Flask(__name__)
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-  
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+ 
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 
-    db.init_app(app)
-    bcrypt.init_app(app)
+r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+pubsub = r.pubsub()
+db.init_app(app)
+bcrypt.init_app(app)
 
+@app.route('/orders', methods=['POST'])
+def create_order():
+    data = request.json
+    new_order = Order(customer_id=data['customer_id'], amount=data['amount'])
+    db.session.add(new_order)
+    db.session.commit()
+
+    r.publish('order_created', json.dumps({'order_id': new_order.id, 'customer_id': new_order.customer_id, 'amount': new_order.amount}))
+
+    return jsonify(new_order.to_dict()), 201
+
+
+def update_order_status(order_id, status):
+    with app.app_context():
+        order = db.session.get(Order, order_id)
+        if order:
+            order.status = status
+            db.session.commit()
+            return jsonify(order.to_dict()), 201
+
+@app.route('/orders/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    order = Order.query.get(order_id)
+    if order:
+        return jsonify({'customer_id': order.customer_id, 'status': order.status, 'amount': order.amount})
+    else:
+        return jsonify({'error': 'Order not found'}), 404
+
+def handle_payment_status_event(data):
+    order_id = data['order_id']
+    status = data['status']
+
+    if status == 'SUCCESS':
+        update_order_status(order_id, 'paid')
+    elif status in ['INSUFFICIENT_FUND', 'TIMEOUT', 'UNKNOWN']:
+        update_order_status(order_id, 'payment_failed')
+
+r.pubsub().subscribe(**{'payment_status': handle_payment_status_event})
+def payment_status_listener():
+    pubsub = r.pubsub()
+    pubsub.subscribe('payment_status')
+
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            data = json.loads(message['data'])
+            handle_payment_status_event(data)
+
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    return app
+    thread = threading.Thread(target=payment_status_listener)
+    thread.start()
 
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(port=port, debug=True, host='0.0.0.0')
+    app.run(debug=True, port=5000)
--- order-service/src/database.py
+++ order-service/src/database.py
@@ -8,16 +8,31 @@ bcrypt = Bcrypt()
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=False, default='pending')
     amount = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'<Order {self.id}>'
-    
     def to_dict(self):
         return {
-            "id": self.id,
-            "customer_id": self.customer_id,
-            "status": self.status,
-            "amount": self.amount
+            'id': self.id,
+            'customer_id': self.customer_id,
+            'status': self.status,
+            'amount': self.amount
         }
+
+# class Order(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     customer_id = db.Column(db.Integer, nullable=False)
+#     status = db.Column(db.String(50), nullable=False)
+#     amount = db.Column(db.Float, nullable=False)
+
+#     def __repr__(self):
+#         return f'<Order {self.id}>'
+    
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "customer_id": self.customer_id,
+#             "status": self.status,
+#             "amount": self.amount
+#         }
+
